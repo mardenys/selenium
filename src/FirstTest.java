6317254c345dfe972e0e2141010e0127757d5148
Marcin <mardenys@gmail.com>
2021-03-07 00:03:52 +0100

    first test

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,20 +1,20 @@
-
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-
 public class FirstTest {
+    //WebDriver driver;
+
     @Test
     public void testOne () {
 
         System.setProperty("webdriver.chrome.driver","src/drivers/win32/chromedriver80/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
 
-        driver.navigate().to("https://www.google.com");
+        driver = new ChromeDriver();
+        driver.navigate().to("https://www.saucedemo.com");
 
-        driver.quit();
+        driver.close();
 
 
     }

d591a2a479f91bd902e283102bd786a858f66179
Marcin <mardenys@gmail.com>
2021-03-07 00:04:04 +0100

    test title

---
+++ src/TestTitle.java
@@ -0,0 +1,24 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TestTitle {
+    WebDriver driver;
+
+    @Test
+    public void  outputTitle() {
+
+        System.setProperty("webdriver.chrome.driver","src/drivers/win32/chromedriver80/chromedriver.exe");
+
+
+        driver = new ChromeDriver();
+        driver.get("https://gmail.com");
+        String currentTitle = driver.getTitle();
+
+        System.out.println("Title " + currentTitle);
+        driver.close();
+
+
+
+    }
+}
